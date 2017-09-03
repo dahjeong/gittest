8d39d902700273a32c7b02c0a6dcc4db59542028
dahjeong <dah.jeong.kim@gmail.com>
2017-09-03 18:58:52 +0900

    commit all files in the local storage

---
+++ Example#1-6.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+x_data = [[73., 80., 75],[93., 88., 93.], [89., 91., 90.], [96., 98., 100.], [73., 66., 70.]]
+y_data = [[152.], [185.], [180.], [196.], [142.]]
+#
+X = tf.placeholder(tf.float32, shape=[None, 3])
+Y = tf.placeholder(tf.floar32, shape=[None, 1])
+
+W = tf.Variable(tf.random_normal([3, 1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+# Hypothesis
+hypothesis = tf.matmul(X, W) + b
